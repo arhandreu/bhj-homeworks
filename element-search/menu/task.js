74478173c84c366c8c9f820bb4063ac35790de48
arhandreu <86781344+arhandreu@users.noreply.github.com>
2022-04-15 17:04:38 +0500

    HW

---
+++ element-search/menu/task.js
@@ -0,0 +1,16 @@
+let menu = Array.from(document.querySelectorAll('.menu_main li'))
+
+menu.forEach(element => {
+    element.onclick = () => {
+        if (element.querySelector('.menu_sub')) {
+                if (document.querySelector('.menu.menu_sub.menu_active')) {                    
+                    document.querySelector('.menu.menu_sub.menu_active').className = 'menu menu_sub'
+                }
+            element.querySelector('.menu_sub').className = 'menu menu_sub menu_active'
+            return false
+        }
+        else {
+            location = element.querySelector('a').href
+        }               
+    }
+})
