6aecb93f2b8c9c3ec97e7a157a9cc5b7806272a9
arhandreu <86781344+arhandreu@users.noreply.github.com>
2022-04-16 11:27:43 +0500

    closeMenu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,13 +2,17 @@ let menu = Array.from(document.querySelectorAll('.menu_main li'))
 
 menu.forEach(element => {
     element.onclick = () => {
-        if (element.querySelector('.menu_sub')) {
+        if (element.querySelector('.menu.menu_sub.menu_active')) {            
+            element.querySelector('.menu.menu_sub.menu_active').className = 'menu menu_sub'
+            return false    
+        }
+        else if (element.querySelector('.menu_sub')) {
                 if (document.querySelector('.menu.menu_sub.menu_active')) {                    
                     document.querySelector('.menu.menu_sub.menu_active').className = 'menu menu_sub'
                 }
-            element.querySelector('.menu_sub').className = 'menu menu_sub menu_active'
+            element.querySelector('.menu_sub').className = 'menu menu_sub menu_active'            
             return false
-        }
+        }               
         else {
             location = element.querySelector('a').href
         }               
