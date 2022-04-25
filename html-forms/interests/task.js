edb9dddbf44ea314d2fb0b820db764601e569a60
arhandreu <86781344+arhandreu@users.noreply.github.com>
2022-04-25 11:41:46 +0500

    HW???

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,13 +1,32 @@
-mainCheck = [...document.querySelectorAll('.interests_active')]
-    .map((check) => check.closest('.interest').querySelector('input'))
+mainCheck = [...document.querySelectorAll('.interest__check')];
+    
 
 function onCheck () {
-    let childChecks = this.closest('.interest').querySelector('.interests_active').querySelectorAll('input')
-    for (let childCheck of [...childChecks]) {
-        childCheck.checked = this.checked
+    if (this.closest('.interest') !== null && this.closest('.interest').querySelector('.interests_active') !== null && this.closest('.interest').querySelector('.interests_active').querySelectorAll('input') !== null) {
+        let childChecks = this.closest('.interest').querySelector('.interests_active').querySelectorAll('input')
+        for (let childCheck of [...childChecks]) {
+            childCheck.checked = this.checked
+        }
     }
-}
+    for (let check of mainCheck) {        
+        if (check.closest('.interest') !== null && check.closest('.interest').querySelector('.interests_active') !== null && check.closest('.interest').querySelector('.interests_active').querySelectorAll('input') !== null) {
+            if ([...check.closest('.interest').querySelector('.interests_active').querySelectorAll('input')].length == [...check.closest('.interest').querySelector('.interests_active').querySelectorAll('input:checked')].length) {
+                check.indeterminate = false
+                check.checked = true
+            }
+            else if ([...check.closest('.interest').querySelector('.interests_active').querySelectorAll('input:checked')].length ===0){
+                check.indeterminate = false
+                check.checked = false
+            } 
+            else if ([...check.closest('.interest').querySelector('.interests_active').querySelectorAll('input')].length > [...check.closest('.interest').querySelector('.interests_active').querySelectorAll('input:checked')].length) {
+                check.indeterminate = true
+            }
+                                   
+        }
+    } 
+} 
 
-mainCheck.forEach(element => {
-    element.addEventListener('change', onCheck)
+mainCheck.forEach(element => {        
+    element.addEventListener('change', onCheck);    
 });
+
