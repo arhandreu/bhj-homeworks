90bde9ac5be5eaff48b3487f08be578c151acf3f
arhandreu <86781344+arhandreu@users.noreply.github.com>
2022-05-20 11:54:24 +0500

    HW

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -2,32 +2,47 @@ const form = document.forms.signin__form
 const user = document.getElementById('user_id')
 const errorAuthor = document.querySelector('.error')
 const formSign = document.querySelector('.signin')
+const exit = document.getElementById('exit')
+
+
+function showWelcome(id) {
+    user.textContent = id
+    formSign.classList.remove('signin_active')
+    user.closest('div').classList.add('welcome_active')    
+}
+
 
 async function sendAutor(){
     const formData = new FormData(form)        
     let send = await fetch(form.action, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'multipart/form-data',            
-        },
+        method: 'POST',        
         body: formData,
       });
       
     json = await send.json()
     
-    if (json['success']) {
-        user.id = json['user_id']
-        user.classList.add('welcome_active')
+    if (json.success) {
+        id = json.user_id
+        showWelcome(id)
+        form.reset()
+        localStorage.setItem('id', id)
+        
     }
-    else {
-        formSign.remove('signin_active')
-        errorAuthor.classList.add('error_active')
-        errorAuthor.textContent = 'Вы не авторизованы!!!'
+    else {        
+        form.reset()
+        alert('Неверный логин/пароль')        
     }
 }
 
+localStorage.getItem('id') ? showWelcome(localStorage.getItem('id')) : null
 
 form.addEventListener('submit', (event) => {
     event.preventDefault()    
     sendAutor()
+})
+
+exit.addEventListener('click', (event) =>{
+    event.preventDefault()
+    localStorage.clear()
+    location.reload()
 })
