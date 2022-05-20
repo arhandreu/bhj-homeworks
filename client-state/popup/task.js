0acb99e9081fa5d598d6ac7b20544e1973eea8f4
arhandreu <86781344+arhandreu@users.noreply.github.com>
2022-05-20 11:55:23 +0500

    HW

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,96 +1,59 @@
-// getCookie(), setCookie(), deleteCookie()
-
-
-// возвращает cookie если есть или undefined
+// получение cookie
 function getCookie(name) {
-
-    var matches = document.cookie.match(new RegExp(
+    const matches = document.cookie.match(new RegExp(
       "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
     ))
     return matches ? decodeURIComponent(matches[1]) : undefined
 }
 
 // уcтанавливает cookie
-function setCookie(name, value, props) {
-
-    props = props || {}
-
-    var exp = props.expires
+function setCookie(name, value, props={}) {    
+    let exp = props.expires
 
     if (typeof exp == "number" && exp) {
-
-        var d = new Date()
-
-        d.setTime(d.getTime() + exp*1000)
-
-        exp = props.expires = d
-
+        const date = new Date()
+        date.setTime(date.getTime() + exp*1000)
+        exp = props.expires = date
     }
 
-    if(exp && exp.toUTCString) { props.expires = exp.toUTCString() }
+    if(exp && exp.toUTCString) { 
+        props.expires = exp.toUTCString() 
+    }
 
     value = encodeURIComponent(value)
+    let updatedCookie = name + "=" + value
 
-    var updatedCookie = name + "=" + value
-
-    for(var propName in props){
-
+    for(const propName in props){
         updatedCookie += "; " + propName
-
-        var propValue = props[propName]
-
-        if(propValue !== true){ updatedCookie += "=" + propValue }
+        const propValue = props[propName]
+        if(propValue !== true) { 
+            updatedCookie += "=" + propValue 
+        }
     }
 
     document.cookie = updatedCookie
-
 }
 
 // удаляет cookie
 function deleteCookie(name) {
-
     setCookie(name, null, { expires: -1 })
-
 }
-/*
-Аргументы:
-
-name
-название cookie
-value
-значение cookie (строка)
-props
-Объект с дополнительными свойствами для установки cookie:
-expires
-Время истечения cookie. Интерпретируется по-разному, в зависимости от типа:
-Если число - количество секунд до истечения.
-Если объект типа Date - точная дата истечения.
-Если expires в прошлом, то cookie будет удалено.
-Если expires отсутствует или равно 0, то cookie будет установлено как сессионное и исчезнет при закрытии браузера.
-path
-Путь для cookie.
-domain
-Домен для cookie.
-secure
-Пересылать cookie только по защищенному соединению.
-*/
-
-
 
 const modal = document.querySelector('#subscribe-modal')
 const buttonClose = document.querySelector('.modal__close_times')
 
-if (getCookie('firstVisit') === undefined) {
+if (getCookie('countVisit') === undefined) {
     modal.classList.add('modal_active')
 }
 else {
-    setCookie('firstVisit', Number(getCookie('firstVisit')) + 1, {expires: 5000} )
+    setCookie('countVisit', Number(getCookie('countVisit')) + 1, {expires: 5000} )
 }
 
 buttonClose.addEventListener('click', (event) => {
     event.preventDefault()
-    setCookie('firstVisit', '1', {expires: 5000} )
+    setCookie('countVisit', '1', {expires: 5000} )
     modal.remove('modal_active')
 })
+
 
 
